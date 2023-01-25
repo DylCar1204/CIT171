550cd57de1ae6a7b2c7799835b0a0fdb149828be
Dylan Carrasco <car19042@byui.edu>
2023-01-24 18:53:00 -0700

    class1

--- manageuser.js
+++ manageuser.js
@@ -11,10 +11,6 @@ function setusername() {
 
 function setuserpassword() {
   password = $("#password").val();
-  var valid = passwordRegEx.exec(password);
-  if (!valid) {
-    alert("Must be 6 digits, upper, lower, number, and symbol");
-  }
 }
 
 function setverifypassword() {
@@ -49,9 +45,10 @@ function checkexpiredtoken(token) {
 }
 
 function oneTimePassword() {
+  const phoneNumber = document.getElementById("username").value;
   $.ajax({
     type: "POST",
-    url: "https://dev.stedi.me/twofactorlogin/4807031541",
+    url: "https://dev.stedi.me/twofactorlogin/" + phoneNumber,
     data: JSON.stringify({ userName, password }),
     contentType: "application/text",
     dataType: "text",
@@ -63,8 +60,11 @@ function userlogin() {
   setusername();
   $.ajax({
     type: "POST",
-    url: "https://dev.stedi.me/login",
-    data: JSON.stringify({ userName, password }),
+    url: "https://dev.stedi.me/twofactorlogin",
+    data: JSON.stringify({
+      phoneNumber: "4807031541",
+      oneTimePassword: "5600",
+    }),
     success: function (data) {
       window.location.href = "/timer.html#" + data; //add the token to the url
     },
